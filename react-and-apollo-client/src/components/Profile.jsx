15a3c8f8ef68b8d5452e1306f4caafb7b2f1b1f5
Anders Nylund <anders.nylund.an@gmail.com>
2018-12-29 15:20:28 +0200

    Implement pagination of repositories

--- react-and-apollo-client/src/components/Profile.jsx
+++ react-and-apollo-client/src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 
 import RepositoryList from './RepositoryList';
 import Loading from './Loading';
@@ -8,16 +8,21 @@ import ErrorMessage from './ErrorMessage';
 import { REPOSITORY_FRAGMENT } from './RepositoryItem';
 
 const GET_CURRENT_USER = gql`
-  {
+  query($cursor: String) {
     viewer {
       repositories(
         first: 5
         orderBy: { direction: DESC, field: STARGAZERS }
+        after: $cursor
       ) {
         edges {
           node {
             ...repository
           }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
         }
       }
       login
@@ -28,18 +33,28 @@ const GET_CURRENT_USER = gql`
   ${REPOSITORY_FRAGMENT}
 `;
 
-const Profile = ({ data, loading, error }) => {
-  if (error) {
-    return <ErrorMessage error={error} />;
-  }
+const Profile = () => (
+  <Query query={GET_CURRENT_USER} notifyOnNetworkStatusChange>
+    {({ data, loading, error, fetchMore }) => {
+      if (error) {
+        return <ErrorMessage error={error} />;
+      }
 
-  const { viewer } = data;
+      const { viewer } = data;
 
-  if (loading || !viewer) {
-    return <Loading />;
-  }
+      if (loading && !viewer) {
+        return <Loading />;
+      }
 
-  return <RepositoryList repositories={viewer.repositories} />;
-};
+      return (
+        <RepositoryList
+          loading={loading}
+          repositories={viewer.repositories}
+          fetchMore={fetchMore}
+        />
+      );
+    }}
+  </Query>
+);
 
-export default graphql(GET_CURRENT_USER)(Profile);
+export default Profile;
--- react-and-apollo-client/src/components/RepositoryList.jsx
+++ react-and-apollo-client/src/components/RepositoryList.jsx
@@ -2,8 +2,31 @@ import React from 'react';
 import { Container, List } from 'semantic-ui-react';
 
 import RepositoryItem from './RepositoryItem';
+import Button from './Button';
+import Loading from './Loading';
 
-const RepositoryList = ({ repositories }) => (
+const updateQuery = (previousResult, { fetchMoreResult }) => {
+  if (!fetchMoreResult) {
+    return previousResult;
+  }
+
+  return {
+    ...previousResult,
+    viewer: {
+      ...previousResult.viewer,
+      repositories: {
+        ...previousResult.viewer.repositories,
+        ...fetchMoreResult.viewer.repositories,
+        edges: [
+          ...previousResult.viewer.repositories.edges,
+          ...fetchMoreResult.viewer.repositories.edges,
+        ],
+      },
+    },
+  };
+};
+
+const RepositoryList = ({ repositories, loading, fetchMore }) => (
   <Container>
     <List divided relaxed>
       {repositories.edges.map(({ node }) => (
@@ -12,6 +35,24 @@ const RepositoryList = ({ repositories }) => (
         </List.Item>
       ))}
     </List>
+    {loading ? (
+      <Loading />
+    ) : (
+      repositories.pageInfo.hasNextPage && (
+        <Button
+          onClick={() =>
+            fetchMore({
+              variables: {
+                cursor: repositories.pageInfo.endCursor,
+              },
+              updateQuery,
+            })
+          }
+        >
+          More repositories
+        </Button>
+      )
+    )}
   </Container>
 );
 
