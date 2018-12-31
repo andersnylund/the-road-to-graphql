4251f29eac45b9dbbed09e46988040bd57c838d9
Anders Nylund <anders.nylund.an@gmail.com>
2018-12-31 11:21:44 +0200

    Add first working version and required node dependencies

--- node-apollo-server/src/index.js
+++ node-apollo-server/src/index.js
@@ -1,1 +1,40 @@
-console.log('Hello World');
+import express from 'express';
+import helmet from 'helmet';
+import { ApolloServer, gql } from 'apollo-server-express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+    age: Int!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => ({
+      username: 'Anders Nylund',
+      age: 24,
+    }),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  // eslint-disable-next-line no-console
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
